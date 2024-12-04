c28d72a1a51ddd27149c77c3a4cf84eb4417715a
MisalSilva <misal.20230183@iit.ac.lk>
2024-12-04 18:38:29 +0530

    Update

--- Backend Ticket Management System/src/main/java/Repository/TicketRepository.java
+++
@@ -1,7 +0,0 @@
-package Repository;
-
-import Model.Ticket;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TicketRepository extends JpaRepository<Ticket, Long> {
-}
--- Backend Ticket Management System/src/main/java/com/example/demo/DemoApplication.java
+++ Backend Ticket Management System/src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,10 @@
 package com.example.demo;
 
-import Configuration.AppConfig;
-import Model.ConfigParameters;
-import MultiThreading.Customer;
-import MultiThreading.TicketPool;
-import MultiThreading.Vendor;
+//import Configuration.AppConfig;
+//import Model.ConfigParameters;
+//import MultiThreading.Customer;
+//import MultiThreading.TicketPool;
+//import MultiThreading.Vendor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -15,12 +15,12 @@ public class DemoApplication {
 
 		SpringApplication.run(DemoApplication.class, args);
 
-		AppConfig appConfig = new AppConfig();
-		ConfigParameters config = appConfig.getConfigFormCLI();
+//		AppConfig appConfig = new AppConfig();
+//		ConfigParameters config = appConfig.getConfigFormCLI();
 
-		TicketPool ticketPool = new TicketPool();
-		new Thread(new Vendor(ticketPool)).start();
-		new Thread(new Customer(ticketPool)).start();
+//		TicketPool ticketPool = new TicketPool();
+//		new Thread(new Vendor(ticketPool)).start();
+//		new Thread(new Customer(ticketPool)).start();
 	}
 
 }
